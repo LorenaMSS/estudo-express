942e0d4deaa4508c7da9aa0435048b60a5708f38
LorenaMss <lorenasouza303@gmail.com>
2021-04-01 11:09:27 -0300

    get post put e delete finalizados

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.get('/projects', (request, response) => {
 
 app.post('/projects', (request, response) => {
   const { title, owner } = request.body;
+
   const project = { id: uuidv4(), title, owner };
 
   projects.push(project); // joga a criação do projeto para o array
@@ -35,15 +36,42 @@ app.post('/projects', (request, response) => {
 });
 
 app.put('/projects/:id', (request, response) => {
-  const params = request.params;
+  const { id } = request.params; // aqui pegamos nosso ID
+  const { title, owner } = request.body; // retornando uma nova informação
 
-  console.log(params);
+  // aqui usamos o findIndex para percorrer todo o array atrás do id
+  //findIndex vai percorrer todos os projetos e toda vez que ele percorrer na variavel project
+  // caso ela satisfeita e retornar true ela vai me retornar o id que estou passando (project=>project.id===id)
+  const projectIndex = projects.findIndex((project) => project.id === id);
 
-  return response.json(['Projeto 5105', 'Projeto 2']);
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Projeto não foi encontrado' });
+  }
+
+  // agora tenho indice vou criar uma nova indormação de projeto
+
+  const project = {
+    id,
+    title,
+    owner,
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
 });
 
 app.delete('/projects/:id', (request, response) => {
-  return response.json(['Projeto 50', 'Projeto 2']);
+  const { id } = request.params;
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Projeto não foi encontrado' });
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.listen(3333);
